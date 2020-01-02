c0ad82ec9657359f3d9af3a3554e3d11b6ede11b
Nassi Harel <nassi9@gmail.com>
2020-01-02 08:48:04 +0200

    fix: taskStatus

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const pipelineStatus = require('./lib/pipeline-status');
 const pipelineType = require('./lib/pipeline-type');
-const tasksStatus = require('./lib/task-status');
+const taskStatus = require('./lib/task-status');
 const verbosityLevel = require('./lib/verbosity-level');
 const buildType = require('./lib/build-type');
 
 module.exports = {
     pipelineStatus,
     pipelineType,
-    tasksStatus,
+    taskStatus,
     verbosityLevel,
     buildType
 };
