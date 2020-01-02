94a2597cd2945790cd0bf1a218e3b4b21aa8c2e4
Nassi Harel <nassi9@gmail.com>
2020-01-02 08:24:30 +0200

    feat: add verbosity-level

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const pipelineStatus = require('./lib/pipeline-status');
 const pipelineType = require('./lib/pipeline-type');
 const tasksStatus = require('./lib/task-status');
+const verbosityLevel = require('./lib/verbosity-level');
 
 module.exports = {
     pipelineStatus,
     pipelineType,
-    tasksStatus
+    tasksStatus,
+    verbosityLevel
 };
