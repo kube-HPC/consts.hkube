35c35187ef0a256af4e53af1bd583c222f6c5fec
Nassi Harel <nassi9@gmail.com>
2020-01-02 08:35:00 +0200

    feat: add buildType

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const pipelineStatus = require('./lib/pipeline-status');
 const pipelineType = require('./lib/pipeline-type');
 const tasksStatus = require('./lib/task-status');
 const verbosityLevel = require('./lib/verbosity-level');
+const buildType = require('./lib/build-type');
 
 module.exports = {
     pipelineStatus,
     pipelineType,
     tasksStatus,
-    verbosityLevel
+    verbosityLevel,
+    buildType
 };
