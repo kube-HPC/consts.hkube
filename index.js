2a3ce8a423b843dec9424d0c6d9778ee9e9fa17c
Nassi Harel <nassi9@gmail.com>
2020-01-05 15:37:58 +0200

    feat: add consts

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const boardStatuses = require('./lib/board-statuses');
+const buildStatuses = require('./lib/build-statuses');
 const pipelineStatuses = require('./lib/pipeline-statuses');
 const pipelineTypes = require('./lib/pipeline-types');
 const taskStatuses = require('./lib/task-statuses');
@@ -5,6 +7,8 @@ const verbosityLevels = require('./lib/verbosity-levels');
 const buildTypes = require('./lib/build-types');
 
 module.exports = {
+    boardStatuses,
+    buildStatuses,
     pipelineStatuses,
     pipelineTypes,
     taskStatuses,
