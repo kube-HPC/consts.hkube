95f3b78a63b013b468855fe9774842bf8fecfa4b
NassiHarel <nassi9@gmail.com>
2020-07-26 10:47:29 +0300

    feat: add state-type

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const boardStatuses = require('./lib/board-statuses');
 const buildStatuses = require('./lib/build-statuses');
+const buildTypes = require('./lib/build-types');
+const graphEdgeTypes = require('./lib/graph-edge-types');
 const pipelineStatuses = require('./lib/pipeline-statuses');
 const pipelineTypes = require('./lib/pipeline-types');
+const retryPolicy = require('./lib/retry-policy');
+const stateType = require('./lib/state-type');
 const taskStatuses = require('./lib/task-statuses');
 const verbosityLevels = require('./lib/verbosity-levels');
-const buildTypes = require('./lib/build-types');
-const retryPolicy = require('./lib/retry-policy');
-const graphEdgeTypes = require('./lib/graph-edge-types');
 
 module.exports = {
     boardStatuses,
     buildStatuses,
+    buildTypes,
+    graphEdgeTypes,
     pipelineStatuses,
     pipelineTypes,
-    taskStatuses,
-    verbosityLevels,
-    buildTypes,
     retryPolicy,
-    graphEdgeTypes
+    stateType,
+    taskStatuses,
+    verbosityLevels
 };
